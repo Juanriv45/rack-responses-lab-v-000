a245ebf328357febd2d1c3d38dbc505f090c5a5e
Juan Rivera <juanriv45@gmail.com>
2018-04-23 04:01:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
     t = Time.new
     t.localtime
     if t.hour >= 12 then
-      resp.write "#{t.hour} Good Afternoon!"
+      resp.write "#Good Afternoon!"
     else
-      resp.write "#{t.hour} Good Morning!"
+      resp.write "#Good Morning!"
     end
 
     resp.finish
