cec3be712fdcf89071621ad81a922bebc525523c
Juan Rivera <juanriv45@gmail.com>
2018-04-23 03:57:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    t = Time.now
+    t = Time.new
 
     if t.hour >= 12 then
       resp.write "#{t.localtime.hour} Good Afternoon!"
