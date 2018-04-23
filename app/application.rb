39ef72f5b5874c309e16f5b50b93bb1ee9a8e788
Juan Rivera <juanriv45@gmail.com>
2018-04-23 03:50:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,17 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     t = Time.now
-    
-    if t >= 12 then
+
+  '  if t >= 12 then
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
-    end
-    
+    end'
+
+    resp.write "#{t}"
     resp.finish
   end
- 
+
 end
